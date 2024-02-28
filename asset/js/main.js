ad3cb8a1e323cc853a0ed986b7bfa15608218156
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-28 15:42:36 +0100

    first trial grid

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,2 +1,18 @@
 // creare bottone che generi una griglia
+const containerE1 = document.getElementById('container');
+const maxCellsNumber = 100;
+const markup = '<div class="box"></div>';
+
+for (let i = 0; i < maxCellsNumber; i++) {
+    containerE1.insertAdjacentHTML('beforeend', markup);
+}
+
+const boxElements = document.getElementsByClassName('box');
+
+for (let i = 0; i < boxElements.length; i++) {
+    const element = boxElements[i];
+    element.addEventListener('click', function() {
+        element.classList.toggle('red');
+    });
+}
 
